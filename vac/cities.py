7a9d910c31804242dbad22c41bb09342d4cc3d0e
Belal Salah <belalsalah74@gmail.com>
2022-10-06 04:10:23 +0200

    edited cities.py to use pathlib

--- vac/cities.py
+++ vac/cities.py
@@ -1,6 +1,9 @@
 import json
+import pathlib
+cities = pathlib.Path('vac/cities.json')
+
 CITY_CHOICES = []
-with open(r'vac\cities.json') as file:
+with open(cities) as file:
     data = json.load(file)[2].get('data')
     CITY_CHOICES = [(city.get('id'), city.get('city_name_en'))
                     for city in data if city.get("governorate_id") == '1']
