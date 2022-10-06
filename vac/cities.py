ada62b44362dbb1b1ad7f6735b3e59b8c21473c5
Belal Salah <belalsalah74@gmail.com>
2022-10-06 04:35:36 +0200

    removed non utf words from cities.json

--- vac/cities.py
+++ vac/cities.py
@@ -6,5 +6,5 @@ CITY_CHOICES = []
 with open(cities) as file:
     data = json.load(file)[2].get('data')
     CITY_CHOICES = [(city.get('id'), city.get('city_name_en'))
-                    for city in data if city.get("governorate_id") == '1']
+                    for city in data if city.get("governorate_id") == '1' or city.get("governorate_id") == '2']
 
